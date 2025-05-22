eb441a1dccfe608b291b78727231e32f99a93e24
evgeny-dav <wild-wild-infa@yandex.ru>
2025-05-22 16:32:48 +0300

    Update CTA.tsx

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -8,12 +8,12 @@ const CTA = () => {
     <section className="bg-gradient-to-r from-[#0F3460] to-[#16213E] text-white py-16">
       <div className="guard-container">
         <div className="max-w-3xl mx-auto text-center">
-          {/*<h2 className="text-3xl md:text-4xl font-bold mb-6" text-gray-700>*/}
-                    <h2 className="text-3xl md:text-4xl font-bold mb-4 gradient-heading">
+          <h2 className="text-3xl md:text-4xl font-bold mb-6" >
+                  {/*  <h2 className="text-3xl md:text-4xl font-bold mb-4 gradient-heading">*/}
 
             Готовы обеспечить безопасность?
           </h2>
-          <p className="text-lg md:text-xl mb-8 text-gray-500">
+          <p className="text-lg md:text-xl mb-8 ">
             Свяжитесь с нами сегодня для бесплатной консультации и оценки стоимости проекта.
             Наши специалисты ответят на все ваши вопросы и подберут оптимальное решение.
           </p>

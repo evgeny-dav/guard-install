ffcc89390c201633b7f2e3f3049e5c063e26c6b0
evgeny-dav <wild-wild-infa@yandex.ru>
2025-05-22 17:18:22 +0300

    Update CTA.tsx

--- src/components/home/CTA.tsx
+++ src/components/home/CTA.tsx
@@ -9,7 +9,7 @@ const CTA = () => {
       <div className="guard-container">
         <div className="max-w-3xl mx-auto text-center">
           <h2 className="text-3xl md:text-4xl font-bold mb-6" >
-                   <h2 className="text-3xl md:text-4xl font-bold mb-4 gradient-heading">
+                  
 
             Готовы обеспечить безопасность?
           </h2>
